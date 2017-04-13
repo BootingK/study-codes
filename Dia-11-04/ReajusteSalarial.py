1624ae476836d9f6ae4ccdf13e8319419cf2e0ab
bootingdesenvolvimento@gmail.com <root@localhost.localdomain>
2017-04-13 00:05:23 -0300

    Exercícios dia 11-04

--- Dia-11-04/ReajusteSalarial.py
+++ Dia-11-04/ReajusteSalarial.py
@@ -1,4 +1,15 @@
-print(1700*100/20000);
+salarioStr = input('Informe seu salário => ');
+
+salarioInt = 0;
+try:
+    salarioInt = int(salarioStr);
+except:
+    print('Digite apenas NÚMEROS');
+    
+if salarioInt <= 1700:
+    print('Seu salário sofreu um reajuste de + %10 por ser maior de 1700,passando para',salarioInt + (salarioInt * 0.10));
+else:
+    print('Seu salário sofreu um reajuste de - %5 por ser maior de 1700,passando para',salarioInt - (salarioInt * 0.05))
 
 
 
